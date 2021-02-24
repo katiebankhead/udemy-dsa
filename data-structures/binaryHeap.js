1c36a317d2ed010d2d4249034c204f46ef2dbe3b
Katie Bankhead <katie@bankheads.net>
2021-02-23 22:24:30 -0700

    Binary Heap extractMax/sinkDown

--- data-structures/binaryHeap.js
+++ data-structures/binaryHeap.js
@@ -28,6 +28,53 @@ class MaxBinaryHeap {
 
         return this;
     }
+
+    extractMax() {
+        // remove old root and replace with last value
+        let max = this.values[0];
+
+        if(this.values.length > 0) {
+            this.values[0] = this.values.pop()
+            this.sinkDown();
+        }
+    
+        return max;
+    }
+
+    sinkDown() {
+        let index = 0;
+        const length = this.values.length;
+        const element = this.values[0];
+
+        while (true) {
+            let leftChildIndex = (2 * index) + 1;
+            let rightChildIndex = (2 * index) + 2;
+            let leftChild;
+            let rightChild;
+            let swapIndex = null;
+
+            if (leftChildIndex < length) {
+                leftChild = this.values[leftChildIndex];
+                if (leftChild > element) {
+                    swapIndex = leftChildIndex;
+                }
+            }
+            if (rightChildIndex < length) {
+                rightChild = this.values[rightChildIndex];
+                if((swapIndex === null && rightChild > element) ||
+                   (swapIndex !== null && rightChild > leftChild)
+                ) {
+                    swapIndex = rightChildIndex;
+                }
+            }
+            if(swapIndex === null) break;
+
+            this.values[index] = this.values[swapIndex];
+            this.values[swapIndex] = element;
+
+            index = swapIndex;
+        }
+    }
 }
 
 let heap = new MaxBinaryHeap();
@@ -36,3 +83,5 @@ let heap = new MaxBinaryHeap();
 // console.log(heap.insert(11));
 // console.log(heap.insert(26));
 console.log(heap.insert(100));
+console.log(heap.extractMax())
+console.log(heap);
